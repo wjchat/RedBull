d278bff925570894caf9e2f77bf410c35a889bb8
Will Chatterson <wjchat@umich.edu>
2018-07-13 21:54:11 -0400

    nav animation, form page

---
+++ rack/js/checkboxes.js
@@ -0,0 +1,38 @@
+let inputs = document.querySelector('.checkboxes').getElementsByTagName('input');
+
+let fakeBoxes = document.querySelector('.checkboxes').getElementsByClassName('fakebox');
+
+
+console.log(inputs);
+console.log(fakeBoxes);
+function checkBox(box){
+    for(let each of fakeBoxes){
+        each.querySelector('.check').classList.add('hide');
+    }    
+    box.querySelector('.check').classList.remove('hide');
+    
+    for (let each of inputs){
+        each.checked = false;
+        if(each.getAttribute('name') === box.getAttribute('name')){
+            each.checked = true;
+            
+            if (each.getAttribute('name') === 'no'){
+                document.querySelector('#invitation').disabled = true;
+                document.querySelector('#invitation').classList.add('disabled');
+                document.querySelector('#invitation-text').classList.add('disabled');
+                
+            }else{
+                document.querySelector('#invitation').disabled = false;    
+                document.querySelector('#invitation').classList.remove('disabled');
+                document.querySelector('#invitation-text').classList.remove('disabled');
+            }
+        }
+    }
+    
+}
+
+for(let each of fakeBoxes){
+    each.onclick = function(){
+        checkBox(each);
+    }
+}
